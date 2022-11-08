cb55d32c48490b0280969f5313193c49c23032c3
EvgenyDavydenko <frontend82@mail.ru>
2022-11-08 13:35:05 +0300

    db config and dump.sql

--- views/shop/index.php
+++ views/shop/index.php
@@ -4,11 +4,12 @@
     <div class="row">
         <div class="col-lg-3">
             <h1 class="my-4">Shop Name</h1>
-            <div class="list-group">
+            <button type="submit" class="btn btn-outline-dark btn-block">Cart</button>
+            <div class="list-group mt-5">
                 <a class="list-group-item" href="#!">Category 1</a>
                 <a class="list-group-item" href="#!">Category 2</a>
                 <a class="list-group-item" href="#!">Category 3</a>
-            </div>
+            </div>            
         </div>
         <div class="col-lg-9">
             <div class="carousel slide my-4" id="carouselExampleIndicators" data-ride="carousel">
@@ -34,13 +35,16 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card h-100">
-                        <a href="#!"><img class="card-img-top" src="https://via.placeholder.com/700x400" alt="..." /></a>
+                        <a href="#!"><img class="card-img-top" src="/img/bay-khou-belaya-obezyana.jpg" alt="..." /></a>
                         <div class="card-body">
                             <h4 class="card-title"><a href="#!">Item One</a></h4>
                             <h5>$24.99</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                         </div>
-                        <div class="card-footer"><small class="text-muted">★ ★ ★ ★ ☆</small></div>
+                        <div class="card-footer d-flex justify-content-around">
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
@@ -51,7 +55,10 @@
                             <h5>$24.99</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
                         </div>
-                        <div class="card-footer"><small class="text-muted">★ ★ ★ ★ ☆</small></div>
+                        <div class="card-footer d-flex justify-content-around">
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
@@ -62,7 +69,10 @@
                             <h5>$24.99</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                         </div>
-                        <div class="card-footer"><small class="text-muted">★ ★ ★ ★ ☆</small></div>
+                        <div class="card-footer d-flex justify-content-around">
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
@@ -73,7 +83,10 @@
                             <h5>$24.99</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                         </div>
-                        <div class="card-footer"><small class="text-muted">★ ★ ★ ★ ☆</small></div>
+                        <div class="card-footer d-flex justify-content-around">
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
@@ -84,7 +97,10 @@
                             <h5>$24.99</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur! Lorem ipsum dolor sit amet.</p>
                         </div>
-                        <div class="card-footer"><small class="text-muted">★ ★ ★ ★ ☆</small></div>
+                        <div class="card-footer d-flex justify-content-around">
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                        </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
@@ -95,7 +111,10 @@
                             <h5>$24.99</h5>
                             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
                         </div>
-                        <div class="card-footer"><small class="text-muted">★ ★ ★ ★ ☆</small></div>
+                        <div class="card-footer d-flex justify-content-around">
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                            <button type="submit" class="btn btn-outline-primary">Primary</button>
+                        </div>
                     </div>
                 </div>
             </div>
