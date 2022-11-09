d19e2dc1551da02722405239b8477e388340e118
EvgenyDavydenko <frontend82@mail.ru>
2022-11-09 14:58:48 +0300

    search items by category

--- controllers/ShopController.php
+++ controllers/ShopController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use yii\web\NotFoundHttpException;
 use yii\data\Pagination;
 use app\models\Good;
 
@@ -43,6 +44,31 @@ class ShopController extends Controller
         //$id = \Yii::$app->request->get('id');
         $good = Good::find()->where(['id' => $id])->one();
         return $this->render('item', ['good' => $good]);
+    }
+
+    public function actionCategory($id)
+    {
+
+        $query = Good::find()->where(['category' => $id]);
+
+        $pagination = new Pagination([
+            'defaultPageSize' => 3,
+            'totalCount' => $query->count(),
+        ]);
+
+        //$id = \Yii::$app->request->get('id');
+        $goods = $query->orderBy('id')        
+        ->offset($pagination->offset)
+        ->limit($pagination->limit)
+        ->all();
+
+        if ($goods === null) {
+            throw new NotFoundHttpException;
+        }
+        return $this->render('index', [
+            'goods' => $goods,
+            'pagination' => $pagination,
+        ]);
     }
     
 
--- widgets/views/menu.php
+++ widgets/views/menu.php
@@ -9,9 +9,13 @@
 </div>
 
 <ul class="list-group mt-5">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <a href="<?= \yii\helpers\Url::to(['shop/index'])?>">Весь чай</a>
+        <span class="badge badge-primary badge-pill">14</span>
+    </li>
     <?php foreach ($cats as $cat) : ?>
     <li class="list-group-item d-flex justify-content-between align-items-center">
-        <a href="#"><?= $cat['name']?></a>
+        <a href="<?= \yii\helpers\Url::to(['shop/category', 'id'=>$cat['id']])?>"><?= $cat['name']?></a>
         <span class="badge badge-primary badge-pill">14</span>
     </li>
     <?php endforeach ?>
